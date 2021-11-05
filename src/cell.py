25a3a087cd9087546185ee456016a73f6aac9ff5
adkamin <minichovaa@gmail.com>
2021-11-05 11:32:33 +0100

    sudoku module finished

--- src/cell.py
+++ src/cell.py
@@ -1,22 +1,18 @@
 class Cell:
     def __init__(self, value, domain, neighbors):
-        self.value = value         # The current value (0 if no value)
-        self.domain = domain       # A list of possible valuse for cells
+        """ Cell initializer """
+        self.value = value              # The current value (0 if no value)
+        self.domain = domain            # A list of possible values for cells
         self.neighbors = neighbors      # A list of all fields that this field is constrained by
 
-    def get_value(self):
-        return self.value
-
-    def set_value(self, n):
-        self.value = n
-
-    def get_domain(self):
-        return self.domain
 
     def remove_from_domain(self, n):
+        """ Removes the given value from the domain, and possibly assigns the last value to the field """
         self.domain = self.domain.remove(n)
         if len(domain) == 1:
             self.value = self.domain[0]
+    
 
     def update_neighbors(self, neighbors):
+        """ Updates the neighbors of cell """
         self.neighbors = neighbors
