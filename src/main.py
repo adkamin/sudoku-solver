a3f725e5dc52ae2b08a2fd5395a0dcb22d59252b
adkamin <minichovaa@gmail.com>
2021-11-05 11:32:41 +0100

    sudoku module finished

--- src/main.py
+++ src/main.py
@@ -4,10 +4,6 @@ def main():
     nr = input("Specify sudoku file (1,2,3,4 or 5): ")
     filename = "Sudoku" + nr + ".txt"
     sudoku = Sudoku(filename)
-    print("neighbors: ")
-    for i in range(len(sudoku.grid[5][5].neighbors)):
-        if sudoku.grid[5][5].neighbors[i].value != 0:
-            print(sudoku.grid[5][5].neighbors[i].value)
     print(sudoku)
     print(sudoku.is_solution())
 
--- src/sudoku.py
+++ src/sudoku.py
@@ -1,18 +1,23 @@
-# Author: Andrea Minichova (s1021688)
+""" Author:  Andrea Minichova (s1021688) 
+    Project: Sudoku solver using AC-3 algorithm """
+
 
 import itertools
 import math
 
 from cell import Cell
 
+
 class Sudoku:
     def __init__(self, filename):
+        """ Sudoku initializer """
         self.grid = self.read_sudoku(filename)
+        self.subsquares = []
         self.init_neighbors()
     
 
-    # Read a sudoku text file into a global sudoku 2D list
     def read_sudoku(self, filename):
+        """ Reads into grid from a specified file """
         grid = [[0 for i in range(9)] for j in range(9)]
         with open(filename) as f:
             for i in range(9):
@@ -24,13 +29,14 @@ class Sudoku:
 
 
     def init_neighbors(self):
+        """ Initializes neighbors for each cell in grid """
         for i in range(9):
             for j in range(9):
                 self.grid[i][j].update_neighbors(self.generate_neighbors(i,j))
 
     
-    # Generate neighbors
     def generate_neighbors(self, row, col):
+        """ Returns all neighbors of a cell (which constrain it) """
         vertical, horizontal, subsquare, neighbors = [], [], [], []
         for i in range(9):
             for j in range(9):
@@ -38,11 +44,12 @@ class Sudoku:
                     vertical.append(self.grid[row][i])
                     horizontal.append(self.grid[j][col])
                 subsquare = self.get_subsquare(row, col)
-        neighbors = list(set(vertical + horizontal + subsquare)) # make into set and back to list to remove duplicates
+        neighbors = list(set(vertical + horizontal + subsquare))
         return neighbors
 
 
     def get_subsquare(self, row, col):
+        """ Returns all neighbors inside the same subsquare """
         subsquare = []
         square_row = math.floor(row/3) * 3
         square_col = math.floor(col/3) * 3
@@ -53,8 +60,8 @@ class Sudoku:
         return subsquare
 
 
-    # Prints a visual representation of sudoku
     def __str__(self):
+        """ Returns string representation of sudoku """
         output = "╔═══════╦═══════╦═══════╗\n"
         for i in range(9):
             if i == 3 or i == 6:
@@ -63,35 +70,46 @@ class Sudoku:
             for j in range(9):
                 if j == 3 or j == 6:
                     output += "║ "
-                output += ". " if self.grid[i][j].get_value() == 0 else chr(self.grid[i][j].get_value() + ord('0')) + " "
+                output += ". " if self.grid[i][j].value == 0 else chr(self.grid[i][j].value + ord('0')) + " "
             output += "║\n"
         output += "╚═══════╩═══════╩═══════╝\n"
         return output
 
 
-    # Returns true, if sudoku is solved, returns false otherwise
     def is_solution(self):
+        """ Returns true if sudoku is solved, returns false otherwise """
         for row in self.grid:
-            if len(set(row)) != len(row):
+            if sum(cell.value for cell in row) != 45:
                 return False
         columns = zip(*self.grid)
         for col in columns:
-            if len(set(col)) != len(col):
+            if sum(cell.value for cell in col) != 45:
                 return False
-        for square in subsquares:
-            if len(set(square)) != len(square):
+        self.subsquares = self.generate_subsquares()
+        for square in self.subsquares:
+            if sum(cell.value for cell in square) != 45:
                 return False
         print("Solved!")
         return True
 
 
-    # Returns true if number is one of possible numbers in given cell
-    def num_possible(self, n, row, col):
-        for i in range(9):
-            if self.grid[row][i] == n or self.grid[i][col] == n:
-                return False
-        square = subsquares[math.floor(row/3) + (3 * math.floor(col/3))]
-        for i in range(9):
-            if square[i] == n:
-                return False
-        return True
+    def generate_subsquares(self):
+        """ Returns all subsquares in a sudoku """
+        subsquares = []
+        for i in range(0, 9, 3):
+            for j in range(0, 9, 3):
+                subsquare = list(itertools.chain.from_iterable(row[j:j+3] for row in self.grid[i:i+3]))
+                subsquares.append(subsquare)
+        return subsquares
+
+
+    # # Returns true if number is one of possible numbers in given cell
+    # def num_possible(self, n, row, col):
+    #     for i in range(9):
+    #         if self.grid[row][i] == n or self.grid[i][col] == n:
+    #             return False
+    #     square = subsquares[math.floor(row/3) + (3 * math.floor(col/3))]
+    #     for i in range(9):
+    #         if square[i] == n:
+    #             return False
+    #     return True
